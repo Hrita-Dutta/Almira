f1f8375629aff1ee2e4d59661cff9f87831e1d72
HritaDutta <hritadutta@gmail.com>
2023-08-05 23:55:08 +0545

    test commit

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDb= async()=> {
     try{
      const res = await mongoose.connect('mongodb://127.0.0.1:27017/Almira');
-     if(res) console.log('connected to mongodb')
+     if(res) console.log('Connected to mongodb')
     }catch(err){
      console.log(err)
     }
