f776b0aedbc2e75bade6a4a12a7de73efac93c63
HritaDutta <hritadutta@gmail.com>
2023-08-06 18:36:14 +0545

    code added for fetching products for future use in product card

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,14 +1,31 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Navbar from '@/Components/Navbar'
-
+import { useEffect,useState } from 'react'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [name,setName] = useState('ram')
+  const [age,setAge] = useState(34)
+
+  const fetchAllProducts = () => {
+
+  }
+
+
+  useEffect(() => {
+    fetchAllProducts()
+  }, [])
+
   return (
     <>
     <Navbar/>
-    <button>Get all products</button>
+    {name} ***** {age}
+    <button className=' bg-yellow-500'
+    onClick={()=>setName(Math.random())}
+    >Click me</button>
+    <button className=' bg-lime-500'
+    onClick={()=>setAge(Math.random())}
+    >change age</button>
     </>
   )
 }
