4ca38da957d404dc3f0421b2f90fef6551be9ef4
HritaDutta <hritadutta@gmail.com>
2023-08-08 20:13:49 +0545

    initialized design of login page

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -17,4 +17,18 @@ const registerNewUser = async(req,res) => {
     
 }
 
+const loginUser = async(req,res) => {
+    
+    //req.body ---> email, password
+
+    //1.Email
+    //2.Email user -> No: No user found
+                    //Yes: compare pass(db, haspass --req.body)
+                    //No: 'Incorrect email or password'
+                    //Yes: 'Generate a token'
+                    // res.json({token, ...})
+    
+}
+
+
 module.exports = {registerNewUser};
