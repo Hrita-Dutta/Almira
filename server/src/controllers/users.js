dae79e4d79d5d1f971d8a8adac8f761ac9fc8fea
HritaDutta <hritadutta@gmail.com>
2023-08-08 17:57:52 +0545

    coded a program if user already exist show 'user already exists' if not create a new user with msg 'user created successfully'

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -1,11 +1,20 @@
 const Users = require('../models/users')
 
 const registerNewUser = async(req,res) => {
+    // User already existed
+    const matched = await Users.exists({email: req.body.email})
+    if(matched){
+        res.status(409).json({
+            msg: 'User Alredy Exists'
+        })
+    }else{
+        await Users.create(req.body)
+        res.status(201).json({
+            msg: 'User Created Successfully'
+        })
+    }
     console.log(req.body)
-    await Users.create(req.body)
-    res.json({
-        msg:'success'
-    })
+    
 }
 
 module.exports = {registerNewUser};
